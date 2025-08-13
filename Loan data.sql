fcb6bbcfa8e4a82a24218d5af64f0aacaf6f616a
Geet Israni <146420325+H16902@users.noreply.github.com>
2025-08-13 20:24:59 +0530

    Delete Loan data.sql

--- Loan data.sql
+++
@@ -1,7 +0,0 @@
-SELECT home_ownership,
-count(id) as Total_Loan_Applications,
-SUM(Loan_Amount) as Total_Funded_Amount,
-sum(total_payment) as Total_Received_Amount
-FROM new_schema.financial_loan
-group by home_ownership
-Order by count(id) DESC; 
